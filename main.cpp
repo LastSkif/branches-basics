0e81961c835e9e54bd0ec37d0441f9ed5947b7c2
Кузьмин Матвей Александрович <ip815s07@lwp05.csc.local>
2019-03-27 12:30:29 +0700

    Initial array and sum

--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-main() {
-	int a[10];
-	return 0;
+main()
+{
+    int a[10], i, sum = 0;
+    for (i = 0; i < 10; i++) {
+        scanf("%d", a[i]);
+        sum += a[i];
+    }
+
+    return 0;
 }
